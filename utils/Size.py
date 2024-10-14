32cc62b5857f3342ea0112c27205168d9d50059c
Q220036 <j.fransolet@student.helmo.be>
2024-10-14 09:55:24 +0200

    Classe Size avec ces méthodes

---
+++ utils/Size.py
@@ -0,0 +1,25 @@
+
+"""Use this class for the size of any object in 3D space"""
+class Size:
+    """
+    Constructor of the class Size.
+
+    width: the width of the size
+    height: the height of the size
+    length: the length of the size
+
+    Raise a ValueError if the width, height or length is negative
+    """
+    def __init__(self, width, height, length):
+        if width < 0 or height < 0 or length < 0:
+            raise ValueError("Width, height and length must be positive")
+        self.__width = width
+        self.__height = height
+        self.__length = length
+
+    """Method to get the volume of the size"""
+    def getVolume(self):
+        return self.__width * self.__height * self.__length
+    """Method to get the width of the size"""
+    def __str__(self):
+        return f"(width :{self.__width}, height :{self.__height}, length :{self.__length})"
