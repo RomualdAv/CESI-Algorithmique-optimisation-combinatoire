0f67c1fdf7568616fd166f7fc67fcb524566aa76
Q220036 <j.fransolet@student.helmo.be>
2024-10-16 10:31:35 +0200

    Test du choix de type de camoins

---
+++ tests/utils/TypeTruckTests.py
@@ -0,0 +1,39 @@
+﻿import sys
+
+from src.utils.TypeTruck import typeOfTruckToUse
+
+sys.path.insert(0, '../src')
+import unittest
+from utils.TypeTruck import TypeTruck
+from utils.TypeBox import TypeBox
+
+class TestTypeTruck(unittest.TestCase):
+
+    def test_should_get_open(self):
+        listnotok = [TypeBox.FRAGILE,TypeBox.ALIMENTAL]
+        listok = [TypeBox.FLAMMABLE, TypeBox.PRESSURIZED]
+
+        self.assertEqual(typeOfTruckToUse(listok), TypeTruck.OPEN)
+        self.assertNotEqual(typeOfTruckToUse(listnotok), TypeTruck.OPEN)
+
+    def test_should_get_refrigerate(self):
+        listnotok = [TypeBox.FLAMMABLE, TypeBox.PRESSURIZED]
+        listok = [TypeBox.FRAGILE,TypeBox.ALIMENTAL]
+
+        self.assertEqual(typeOfTruckToUse(listok), TypeTruck.REFRIGERATE)
+        self.assertNotEqual(typeOfTruckToUse(listnotok), TypeTruck.REFRIGERATE)
+
+    def test_should_get_watertight(self):
+        listnotok = [TypeBox.RADIOACTIVE]
+        listok = [TypeBox.TOXIC, TypeBox.CORROSIVE]
+
+        self.assertEqual(typeOfTruckToUse(listok), TypeTruck.WATERTIGHT)
+        self.assertNotEqual(typeOfTruckToUse(listnotok), TypeTruck.WATERTIGHT)
+
+    def test_should_get_plated(self):
+        listok = [TypeBox.TOXIC, TypeBox.RADIOACTIVE,TypeBox.FRAGILE]
+
+        self.assertEqual(typeOfTruckToUse(listok), TypeTruck.PLATED)
+
+if __name__ == '__main__':
+    unittest.main()
