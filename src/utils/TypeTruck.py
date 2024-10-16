c2d0279d8b391f05edcfc2dcdf0563a24b2e41f2
Q220036 <j.fransolet@student.helmo.be>
2024-10-16 10:21:19 +0200

    Correction de l'algo de choix du camoin

--- src/utils/TypeTruck.py
+++ src/utils/TypeTruck.py
@@ -1,28 +1,30 @@
 ﻿from enum import Enum
 import TypeBox
+from error import TruckError
 
 listNotPosible = (
-            (TypeBox.FLAMMABLE, TypeBox.EXPLOSIVE, TypeBox.PRESSURIZED, TypeBox.FRAGILE),
-            (TypeBox.ALIMENTAL, TypeBox.PRESSURIZED, TypeBox.FRAGILE),
-            (TypeBox.ALIMENTAL, TypeBox.FLAMMABLE, TypeBox.EXPLOSIVE, TypeBox.TOXIC, TypeBox.CORROSIVE,
-             TypeBox.OXIDIZING, TypeBox.FRAGILE),
-            (TypeBox.ALIMENTAL, TypeBox.FLAMMABLE, TypeBox.EXPLOSIVE, TypeBox.TOXIC, TypeBox.RADIOACTIVE,
-             TypeBox.CORROSIVE, TypeBox.OXIDIZING, TypeBox.PRESSURIZED, TypeBox.FRAGILE)
+            (TypeBox.ALIMENTAL,TypeBox.TOXIC, TypeBox.RADIOACTIVE, TypeBox.CORROSIVE,
+             TypeBox.OXIDIZING)
+            (TypeBox.FLAMMABLE, TypeBox.EXPLOSIVE, TypeBox.TOXIC, TypeBox.RADIOACTIVE, TypeBox.CORROSIVE,
+             TypeBox.OXIDIZING, TypeBox.PRESSURIZED),
+            (TypeBox.RADIOACTIVE,TypeBox.PRESSURIZED),
+            ()
         )
 """
 This function returns the type of truck to use.
 """
 def typeOfTruckToUse(listtypebox):
-    lengthlisttypebox = len(listtypebox)
-    if listtypebox.contains(listNotPosible[0] and lengthlisttypebox <= len(listNotPosible[0])):
+    if not listtypebox.contains(listNotPosible[0]):
         return TypeTruck.OPEN
-    elif listtypebox.contains(listNotPosible[1] and lengthlisttypebox <= len(listNotPosible[1])):
+    elif not listtypebox.contains(listNotPosible[1]):
         return TypeTruck.REFRIGERATE
-    elif listtypebox.contains(listNotPosible[2] and lengthlisttypebox <= len(listNotPosible[2])):
+    elif not listtypebox.contains(listNotPosible[2]):
         return TypeTruck.WATERTIGHT
-    elif listtypebox.contains(listNotPosible[3] and lengthlisttypebox <= len(listNotPosible[3])):
+    elif not listtypebox.contains(listNotPosible[3]):
         return TypeTruck.PLATED
-    return TypeTruck.OPEN
+
+    #Impossible to determine the type of truck
+    raise TruckError("Impossible to determine the type of truck")
 """
 This class is an enumeration of the types of trucks.
 """
---
+++ src/utils/error.py
@@ -0,0 +1,2 @@
+﻿class TruckError(Exception):
+    pass
