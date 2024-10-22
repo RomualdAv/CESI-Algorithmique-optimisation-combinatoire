2dfacf00599f436b426b34ec393b7ed1789116d1
Romuald la roulade <romuald.aversenq@gmail.com>
2024-10-22 12:55:50 +0200

    update

--- src/utils/Truck.py
+++ src/utils/Truck.py
@@ -18,38 +18,48 @@ Attributes:
 
 class Truck:
     def __init__(self, name:str, size:Size, truck_type:TypeTruck):
-        self.name = name
-        self.size = size
-        self.type = truck_type
-        self.fret = []
+        self.__name = name
+        self.__size = size
+        self.__type = truck_type
+        self.__fret = []
     
+    """Get methods"""
+    def get_name(self):
+        return self.__name
+    
+    def get_size(self):
+        return self.__size
+    
+    def get_type(self):
+        return self.__type
+
     """Method that return the total weight the truck is currently transporting"""
     def getCurrentWeight(self):
         current_weight = 0
-        if self.fret == []:
+        if self.__fret == []:
             raise TruckError("Truck does not contain any fret")
         else:
-            return sum(box.size for box in self.fret)
+            return sum(box.size for box in self.__fret)
 
     """Method that return a boolean value to know if a box size < truck capacity"""
     def canContain(self, box:Box):
-        return box.size <= (self.size - self.get_current_weight())
+        return box.size <= (self.__size - self.get_current_weight())
 
     """Method that return a boolean value to know if the truck is full"""
     def isFull(self):
-        return self.size == self.get_current_weight()
+        return self.__size == self.get_current_weight()
     
     """Method that substracte a box from the truck"""
     def conveyBox(self, id_box:UUID):
-        for box in self.fret:
+        for box in self.__fret:
             if box.id_box == id_box:
-                self.fret.remove(box)
+                self.__fret.remove(box)
                 return
         raise BoxIDError(f"{id_box} not found")
     
     """Methot that add a new set of boxes in the truck"""
     def addFret(self, fret:list[Box]):
         if fret is None:
-            self.fret = fret
+            self.__fret = fret
             return
         raise TruckError("A fret is already set in the truck")
