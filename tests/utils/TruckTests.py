693f8777f0f2944ce4b30dc6eb3158f6172edf85
Q220036 <j.fransolet@student.helmo.be>
2024-10-22 20:14:53 +0200

    Correction des tests unitaires du truck

--- tests/utils/TruckTests.py
+++ tests/utils/TruckTests.py
@@ -16,52 +16,61 @@ class TestTruck(unittest.TestCase):
         self.size = Size(100,100,100)
         self.truck_type = TypeTruck.PLATED
         self.truck = Truck("Truck 1", self.size, self.truck_type)
-        self.box1 = Box(uuid4(), Depot(1,"Lidl",True,DeliveryWindow(datetime.time,datetime.time)),Size(10,10,10),TypeBox.FRAGILE)
-        self.box2 = Box(uuid4(), Depot(2,"Test",False,DeliveryWindow(datetime.time,datetime.time)),Size(20,20,20),TypeBox.ALIMENTAL)
+        self.depot1 = Depot(1,"Lidl",True,DeliveryWindow(datetime.datetime(2023,12,1,8,0),datetime.datetime(2023,12,1,18,0)))
+        self.depot2 = Depot(2,"Test",False,DeliveryWindow(datetime.datetime(2023,12,1,8,0),datetime.datetime(2023,12,1,18,0)))
+        self.box1 = Box(uuid4(), self.depot1,Size(10,10,10),TypeBox.FRAGILE)
+        self.box2 = Box(uuid4(), self.depot2,Size(20,20,20),TypeBox.ALIMENTAL)
 
-    def test_get_name(self):
+    def test_get_name_when_all_is_good(self):
         self.assertEqual(self.truck.get_name(), "Truck 1")
 
-    def test_get_size(self):
+    def test_get_size_when_all_is_good(self):
         self.assertEqual(self.truck.get_size(), self.size)
 
-    def test_get_type(self):
+    def test_get_type_when_all_is_good(self):
         self.assertEqual(self.truck.get_type(), self.truck_type)
 
-    def test_get_current_weight_empty(self):
+    def test_get_current_weight_when_list_is_empty(self):
         with self.assertRaises(TruckError):
             self.truck.getCurrentWeight()
 
-    def test_get_current_weight(self):
-        self.truck.addFret([self.box1])
-        self.assertEqual(self.truck.getCurrentWeight(), 10)
+    def test_get_current_weight_when_you_have_add_one_box(self):
+        self.truck.addFret(self.box1)
+        self.assertEqual(self.truck.getCurrentWeight(), 1000)
+        self.truck.conveyBox(self.box1.getIdBox())
 
-    def test_can_contain(self):
-        self.truck.addFret([self.box1])
-        self.assertTrue(self.truck.canContain(self.box2))
+    def test_fret_contain_box1_when_its_in(self):
+        self.truck.addFret(self.box1)
+        self.assertTrue(self.truck.canContain(self.box1))
+        self.truck.conveyBox(self.box1.getIdBox())
 
-    def test_is_full(self):
-        self.truck.addFret([Box(uuid4(), Size(100,100,100),TypeBox.ALIMENTAL)])
+    def test_truck_is_full_when_its_full(self):
+        uuid = uuid4()
+        self.truck.addFret(Box(uuid,self.depot1, Size(100,100,100),TypeBox.ALIMENTAL))
         self.assertTrue(self.truck.isFull())
+        self.truck.conveyBox(uuid)
 
     def test_convey_box(self):
-        self.truck.addFret([self.box1])
+        self.truck.addFret(self.box1)
         self.truck.conveyBox(self.box1.getIdBox())
         with self.assertRaises(TruckError):
             self.truck.getCurrentWeight()
 
-    def test_convey_box_not_found(self):
+    def test_convey_box_not_found_when_it_have_not_box(self):
         with self.assertRaises(BoxIDError):
             self.truck.conveyBox(uuid4())
 
     def test_add_fret(self):
-        self.truck.addFret([self.box1, self.box2])
-        self.assertEqual(self.truck.getCurrentWeight(), 30)
+        self.truck.addFret(self.box1)
+        self.truck.addFret(self.box2)
+        self.assertEqual(self.truck.getCurrentWeight(), 9000)
+        self.truck.conveyBox(self.box1.getIdBox())
+        self.truck.conveyBox(self.box2.getIdBox())
 
-    def test_add_fret_already_set(self):
-        self.truck.addFret([self.box1])
-        with self.assertRaises(TruckError):
-            self.truck.addFret([self.box2])
+    def test_add_fret_when_its_already_set(self):
+        self.truck.addFret(self.box1)
+        self.truck.addFret(self.box1)
+        self.assertEqual(self.truck.getCurrentWeight(), 1000)
 
 if __name__ == '__main__':
     unittest.main()
