5e1cd59fb1750edfcffefeba554a04d06a98f884
Q220036 <j.fransolet@student.helmo.be>
2024-10-22 20:14:36 +0200

    Correction de la classe truck

--- src/utils/Truck.py
+++ src/utils/Truck.py
@@ -1,8 +1,8 @@
 from uuid import UUID
-from src.utils.Size import Size
-from src.utils.TypeTruck import TypeTruck
-from src.utils.Box import Box
-from src.utils.error import TruckError, BoxIDError
+from .Size import Size
+from .TypeTruck import TypeTruck
+from .Box import Box
+from .error import TruckError, BoxIDError
 
 """
 This class represents a Truck object.
@@ -21,7 +21,7 @@ class Truck:
         self.__name = name
         self.__size = size
         self.__type = truck_type
-        self.__fret = []
+        self.__fret = set()
     
     """Get methods"""
     def get_name(self)->str:
@@ -34,8 +34,7 @@ class Truck:
         return self.__type
 
     """Method that return the total weight the truck is currently transporting"""
-    def getCurrentWeight(self):
-        current_weight = 0
+    def getCurrentWeight(self)->int:
         if not self.__fret:
             raise TruckError("Truck does not contain any fret")
         else:
@@ -47,19 +46,16 @@ class Truck:
 
     """Method that return a boolean value to know if the truck is full"""
     def isFull(self) -> bool:
-        return self.__size == self.getCurrentWeight()
+        return self.__size.getVolume() == self.getCurrentWeight()
     
     """Method that substracte a box from the truck"""
     def conveyBox(self, id_box:UUID):
         for box in self.__fret:
-            if box.id_box == id_box:
+            if box.getIdBox() == id_box:
                 self.__fret.remove(box)
                 return
         raise BoxIDError(f"{id_box} not found")
     
     """Methot that add a new set of boxes in the truck"""
-    def addFret(self, fret:list[Box]):
-        if fret is None:
-            self.__fret = fret
-            return
-        raise TruckError("A fret is already set in the truck")
+    def addFret(self, fret:Box):
+        self.__fret.add(fret)
