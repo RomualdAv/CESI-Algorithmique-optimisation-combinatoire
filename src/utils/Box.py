d005eba354be7cf2d74a8430f0f6af1f4c61737f
Q220036 <j.fransolet@student.helmo.be>
2024-10-22 11:24:05 +0200

    Ajout de sécurité sur la Box

--- src/utils/Box.py
+++ src/utils/Box.py
@@ -1,15 +1,32 @@
 import uuid
-class box:
-    def __init__(self, destination, size, delivery_window,type_box):
-        self.id_box= uuid.uuid4() # generation d'identifiant unique pour une boite
+
+from src.utils.DeliveryWindow import DeliveryWindow
+from src.utils.Depot import Depot
+from src.utils.Size import Size
+from src.utils.TypeBox import TypeBox
+
+"""
+This class represents a Box object.
+
+Attributes:
+    id_box (UUID): Box ID
+    destination (Depot): Destination of the box
+    size (Size): Size of the box
+    delivery_window (DeliveryWindow): Delivery window of the box
+    type (TypeBox): Type of the box
+"""
+class Box:
+    def __init__(self, id_box,destination, size, delivery_window,type_box):
+        if not isinstance(destination, Depot) or not isinstance(id_box, uuid.UUID) or not isinstance(destination, Depot) or not isinstance(size, Size) or not isinstance(delivery_window, DeliveryWindow) or not isinstance(type_box, TypeBox):
+            raise TypeError("Destination must be a Depot object")
+        self.id_box= id_box
         self.destination= destination 
         self.size= size
         self.delivery_window= delivery_window
         self.type = type_box 
     
     def __str__(self):
-        return f"Box ID: {self.id_box}, Destination:{self.destination.name},
-        size: { self.size}, Delivery window: { self.delivery_window}, Type: {self.type.name}"
+        return f"Box ID: {self.id_box}, Destination:{self.destination.name},size: { self.size}, DeliveryWindow: { self.delivery_window}, Type: {self.type.name}"
     
         
        
